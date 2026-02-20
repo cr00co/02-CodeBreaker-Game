9caff16783428d7df38da02d5e8ac14e59d01542
Anton <antonpuh1162@gmail.com>
2026-02-20 14:19:34 +0200

    Initial Commit

---
+++ CodeBreaker-Game/CodeBreaker_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreaker_GameApp.swift
+//  CodeBreaker-Game
+//
+//  Created by Anton on 20/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreaker_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
